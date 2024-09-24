fecebf56a6f384d16a522cb0d9e193bdfd278059
hatchikuji <swannbrillant@gmail.com>
2024-09-24 14:43:57 +0200

    Initial commit

---
+++ Calculatrice/Calculatrice.Designer.vb
@@ -0,0 +1,300 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Calculatrice
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.ButtonE = New System.Windows.Forms.Button()
+        Me.ButtonP = New System.Windows.Forms.Button()
+        Me.ButtonDel = New System.Windows.Forms.Button()
+        Me.ButtonM = New System.Windows.Forms.Button()
+        Me.ButtonV = New System.Windows.Forms.Button()
+        Me.ButtonMU = New System.Windows.Forms.Button()
+        Me.Button0 = New System.Windows.Forms.Button()
+        Me.ButtonN = New System.Windows.Forms.Button()
+        Me.ButtonD = New System.Windows.Forms.Button()
+        Me.Ecran = New System.Windows.Forms.TextBox()
+        Me.ButtonRESET = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.0!)
+        Me.Button1.Location = New System.Drawing.Point(171, 432)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(115, 92)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.0!)
+        Me.Button2.Location = New System.Drawing.Point(292, 432)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(115, 92)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.0!)
+        Me.Button3.Location = New System.Drawing.Point(413, 432)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(115, 92)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.0!)
+        Me.Button4.Location = New System.Drawing.Point(171, 334)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(115, 92)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "4"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.0!)
+        Me.Button5.Location = New System.Drawing.Point(292, 334)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(115, 92)
+        Me.Button5.TabIndex = 4
+        Me.Button5.Text = "5"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.0!)
+        Me.Button6.Location = New System.Drawing.Point(413, 334)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(115, 92)
+        Me.Button6.TabIndex = 5
+        Me.Button6.Text = "6"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.0!)
+        Me.Button7.Location = New System.Drawing.Point(171, 236)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(115, 92)
+        Me.Button7.TabIndex = 6
+        Me.Button7.Text = "7"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.0!)
+        Me.Button8.Location = New System.Drawing.Point(292, 236)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(115, 92)
+        Me.Button8.TabIndex = 7
+        Me.Button8.Text = "8"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.0!)
+        Me.Button9.Location = New System.Drawing.Point(413, 236)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(115, 92)
+        Me.Button9.TabIndex = 8
+        Me.Button9.Text = "9"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'ButtonE
+        '
+        Me.ButtonE.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.0!)
+        Me.ButtonE.Location = New System.Drawing.Point(662, 530)
+        Me.ButtonE.Name = "ButtonE"
+        Me.ButtonE.Size = New System.Drawing.Size(115, 92)
+        Me.ButtonE.TabIndex = 9
+        Me.ButtonE.Text = "="
+        Me.ButtonE.UseVisualStyleBackColor = True
+        '
+        'ButtonP
+        '
+        Me.ButtonP.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.0!)
+        Me.ButtonP.Location = New System.Drawing.Point(662, 432)
+        Me.ButtonP.Name = "ButtonP"
+        Me.ButtonP.Size = New System.Drawing.Size(115, 92)
+        Me.ButtonP.TabIndex = 10
+        Me.ButtonP.Text = "+"
+        Me.ButtonP.UseVisualStyleBackColor = True
+        '
+        'ButtonDel
+        '
+        Me.ButtonDel.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!)
+        Me.ButtonDel.Location = New System.Drawing.Point(783, 530)
+        Me.ButtonDel.Name = "ButtonDel"
+        Me.ButtonDel.Size = New System.Drawing.Size(115, 92)
+        Me.ButtonDel.TabIndex = 11
+        Me.ButtonDel.Text = "DEL"
+        Me.ButtonDel.UseVisualStyleBackColor = True
+        '
+        'ButtonM
+        '
+        Me.ButtonM.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.0!)
+        Me.ButtonM.Location = New System.Drawing.Point(783, 432)
+        Me.ButtonM.Name = "ButtonM"
+        Me.ButtonM.Size = New System.Drawing.Size(115, 92)
+        Me.ButtonM.TabIndex = 12
+        Me.ButtonM.Text = "-"
+        Me.ButtonM.UseVisualStyleBackColor = True
+        '
+        'ButtonV
+        '
+        Me.ButtonV.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.0!)
+        Me.ButtonV.Location = New System.Drawing.Point(292, 530)
+        Me.ButtonV.Name = "ButtonV"
+        Me.ButtonV.Size = New System.Drawing.Size(115, 92)
+        Me.ButtonV.TabIndex = 13
+        Me.ButtonV.Text = "."
+        Me.ButtonV.UseVisualStyleBackColor = True
+        '
+        'ButtonMU
+        '
+        Me.ButtonMU.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.0!)
+        Me.ButtonMU.Location = New System.Drawing.Point(662, 334)
+        Me.ButtonMU.Name = "ButtonMU"
+        Me.ButtonMU.Size = New System.Drawing.Size(115, 92)
+        Me.ButtonMU.TabIndex = 14
+        Me.ButtonMU.Text = "❌"
+        Me.ButtonMU.UseVisualStyleBackColor = True
+        '
+        'Button0
+        '
+        Me.Button0.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.0!)
+        Me.Button0.Location = New System.Drawing.Point(171, 530)
+        Me.Button0.Name = "Button0"
+        Me.Button0.Size = New System.Drawing.Size(115, 92)
+        Me.Button0.TabIndex = 15
+        Me.Button0.Text = "0"
+        Me.Button0.UseVisualStyleBackColor = True
+        '
+        'ButtonN
+        '
+        Me.ButtonN.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.0!)
+        Me.ButtonN.Location = New System.Drawing.Point(413, 530)
+        Me.ButtonN.Name = "ButtonN"
+        Me.ButtonN.Size = New System.Drawing.Size(115, 92)
+        Me.ButtonN.TabIndex = 16
+        Me.ButtonN.Text = "(-)"
+        Me.ButtonN.UseVisualStyleBackColor = True
+        '
+        'ButtonD
+        '
+        Me.ButtonD.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.0!)
+        Me.ButtonD.Location = New System.Drawing.Point(783, 334)
+        Me.ButtonD.Name = "ButtonD"
+        Me.ButtonD.Size = New System.Drawing.Size(115, 92)
+        Me.ButtonD.TabIndex = 17
+        Me.ButtonD.Text = "÷"
+        Me.ButtonD.UseVisualStyleBackColor = True
+        '
+        'Ecran
+        '
+        Me.Ecran.BackColor = System.Drawing.Color.White
+        Me.Ecran.Enabled = False
+        Me.Ecran.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.0!)
+        Me.Ecran.Location = New System.Drawing.Point(122, 28)
+        Me.Ecran.Multiline = True
+        Me.Ecran.Name = "Ecran"
+        Me.Ecran.Size = New System.Drawing.Size(447, 156)
+        Me.Ecran.TabIndex = 18
+        '
+        'ButtonRESET
+        '
+        Me.ButtonRESET.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.0!)
+        Me.ButtonRESET.Location = New System.Drawing.Point(662, 236)
+        Me.ButtonRESET.Name = "ButtonRESET"
+        Me.ButtonRESET.Size = New System.Drawing.Size(236, 92)
+        Me.ButtonRESET.TabIndex = 19
+        Me.ButtonRESET.Text = "RESET"
+        Me.ButtonRESET.UseVisualStyleBackColor = True
+        '
+        'Calculatrice
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(953, 648)
+        Me.Controls.Add(Me.ButtonRESET)
+        Me.Controls.Add(Me.Ecran)
+        Me.Controls.Add(Me.ButtonD)
+        Me.Controls.Add(Me.ButtonN)
+        Me.Controls.Add(Me.Button0)
+        Me.Controls.Add(Me.ButtonMU)
+        Me.Controls.Add(Me.ButtonV)
+        Me.Controls.Add(Me.ButtonM)
+        Me.Controls.Add(Me.ButtonDel)
+        Me.Controls.Add(Me.ButtonP)
+        Me.Controls.Add(Me.ButtonE)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Calculatrice"
+        Me.Text = "Calculatrice"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents ButtonE As Button
+    Friend WithEvents ButtonP As Button
+    Friend WithEvents ButtonDel As Button
+    Friend WithEvents ButtonM As Button
+    Friend WithEvents ButtonV As Button
+    Friend WithEvents ButtonMU As Button
+    Friend WithEvents Button0 As Button
+    Friend WithEvents ButtonN As Button
+    Friend WithEvents ButtonD As Button
+    Friend WithEvents Ecran As TextBox
+    Friend WithEvents ButtonRESET As Button
+End Class
---
+++ Calculatrice/Calculatrice.vb
@@ -0,0 +1,187 @@
+﻿Imports System.Data
+Imports System.Linq.Expressions
+Imports System.Text.RegularExpressions
+
+Public Class Calculatrice
+    Dim _chaine As String
+    Private Sub Calculatrice_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        _chaine = ""
+    End Sub
+
+    Private Sub Button0_Click(sender As Object, e As EventArgs) Handles Button0.Click
+        _chaine += "0"
+        ActualiseChaine()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        _chaine += "1"
+        ActualiseChaine()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        _chaine += "2"
+        ActualiseChaine()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        _chaine += "3"
+        ActualiseChaine()
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        _chaine += "4"
+        ActualiseChaine()
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        _chaine += "5"
+        ActualiseChaine()
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        _chaine += "6"
+        ActualiseChaine()
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        _chaine += "7"
+        ActualiseChaine()
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        _chaine += "8"
+        ActualiseChaine()
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        _chaine += "9"
+        ActualiseChaine()
+    End Sub
+
+    Private Sub ButtonN_Click(sender As Object, e As EventArgs) Handles ButtonN.Click
+        _chaine += "-"
+        ActualiseChaine()
+    End Sub
+
+    Private Sub ButtonV_Click(sender As Object, e As EventArgs) Handles ButtonV.Click
+        _chaine += "."
+        ActualiseChaine()
+    End Sub
+
+    Private Sub ButtonD_Click(sender As Object, e As EventArgs) Handles ButtonD.Click
+        _chaine += "/"
+        ActualiseChaine()
+    End Sub
+
+    Private Sub ButtonMU_Click(sender As Object, e As EventArgs) Handles ButtonMU.Click
+        _chaine += "*"
+        ActualiseChaine()
+    End Sub
+
+    Private Sub ButtonM_Click(sender As Object, e As EventArgs) Handles ButtonM.Click
+        _chaine += "-"
+        ActualiseChaine()
+    End Sub
+
+    Private Sub ButtonP_Click(sender As Object, e As EventArgs) Handles ButtonP.Click
+        _chaine += "+"
+        ActualiseChaine()
+    End Sub
+
+    Private Sub ButtonRESET_Click(sender As Object, e As EventArgs) Handles ButtonRESET.Click
+        Reset()
+        ActualiseChaine()
+    End Sub
+
+    Private Sub Reset()
+        _chaine = ""
+    End Sub
+
+    Private Sub ActualiseChaine()
+        ' Vérifie si l'écran affiche une erreur (NaN)
+        If Ecran.Text = "NaN" Then
+            ' Ne pas modifier l'écran s'il y a une erreur
+            Return
+        End If
+
+        ' Logique normale pour actualiser l'écran
+        Ecran.Text = _chaine
+    End Sub
+
+
+    Private Function Calcul(chaine As String) As Double
+        Try
+            ' Vérifie le contenu de la chaîne
+            Console.WriteLine("Contenu de la chaîne : " & chaine)
+
+            ' Évalue l'expression dans _chaine
+            Dim result As Object = New DataTable().Compute(chaine, Nothing)
+
+            ' Vérifie si le résultat est numérique
+            If IsNumeric(result) Then
+                Dim numericResult As Double = Convert.ToDouble(result)
+
+                ' Vérifie si le résultat est Infini ou NaN
+                If Double.IsInfinity(numericResult) OrElse Double.IsNaN(numericResult) Then
+                    Throw New DivideByZeroException("Division par zéro détectée.")
+                End If
+
+                Return numericResult
+            Else
+                Throw New Exception("Le résultat n'est pas un nombre valide.")
+            End If
+
+        Catch ex As DivideByZeroException
+            ' Affiche le message d'erreur dans la console
+            Console.WriteLine("Erreur : " & ex.Message)
+            ' Propager l'exception pour qu'elle soit capturée dans le bloc Try...Catch de l'appelant
+            Throw
+        Catch ex As Exception
+            ' Affiche le message d'erreur dans la console
+            Console.WriteLine("Erreur : " & ex.Message)
+            ' Propager d'autres exceptions
+            Throw
+        End Try
+    End Function
+
+    
+    Private Sub ButtonDel_Click(sender As Object, e As EventArgs) Handles ButtonDel.Click
+        Dim chaineTemp As String = ""
+        For i = 0 To Len(_chaine) - 2
+            chaineTemp &= _chaine(i)
+        Next
+        _chaine = chaineTemp
+        ActualiseChaine()
+    End Sub
+
+    Private Sub ButtonE_Click(sender As Object, e As EventArgs) Handles ButtonE.Click
+        ' Vérifie si l'écran affiche "NaN" pour réinitialiser la chaîne
+        If Ecran.Text = "NaN" Then
+            _chaine = ""
+            Ecran.Text = ""
+        End If
+
+        Dim result As Double
+        Try
+            ' Appelle la fonction Calcul pour évaluer l'expression
+            result = Calcul(_chaine)
+
+            ' Convertit le résultat en chaîne pour l'assigner à _chaine
+            _chaine = result.ToString()
+
+            ' Affiche le résultat dans l'écran
+            Ecran.Text = _chaine
+        Catch ex As DivideByZeroException
+            ' Si une division par zéro est détectée, affiche "NaN"
+            Console.WriteLine("Division par zéro détectée. Affichage de NaN.")
+            Ecran.Text = "NaN"
+        Catch ex As Exception
+            ' Affiche tout autre message d'erreur
+            Console.WriteLine("Erreur : " & ex.Message)
+            Ecran.Text = ex.Message
+        End Try
+
+        ' Actualise l'affichage de la chaîne
+        ActualiseChaine()
+    End Sub
+End Class
